b3f22c8bb37afcfdaab0d9a0939bac38b7a014e6
CrescentLuo <crescentluozheng@gmail.com>
2021-12-08 17:52:18 -0500

    allow user to set spcific delimiter

--- utils/printh.sh
+++ utils/printh.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-
-while getopts "hf:" option; do
+delimiter="\t"
+while getopts ":d:hf:" option; do
 	case ${option} in 
 		h ) # display help
 			echo "Usage:"
 			echo -e "\tprinth file\t\tprint the header the file."
 			echo -e "\tprinth -h\t\tdisplay the help message."
+			echo -e "\tprinth -d\t\tset delimiter, default delimiter is tab."
 			echo -e "\tprinth -f\t\tdisplay header and the first line."
 			exit 0
 			;;
+		d )
+			delimiter=$OPTARG;
+			echo $delimiter;
+      		;;
 		f )
 			shift
 			file=$1;shift
 			firstline=`sed -n 2p $file`
-			head -n1 $file | awk -F "\t" -v firstline="$firstline" '{OFS="\t";split(firstline,a,"\t");for(i=1;i<=NF;i++){print i,$i,a[i]}}' 
+			head -n1 $file | awk -F ${delimiter} -v firstline="$firstline" '{OFS="\t";split(firstline,a,"\t");for(i=1;i<=NF;i++){print i,$i,a[i]}}' 
 			exit 0
 			;;
+		
 		\? )
 			echo "Invalid Option: -$OPTARG" 1>&2
           	exit 1
@@ -29,4 +35,4 @@ done
 shift $((OPTIND -1))
 
 file=$1;shift
-head -n1 $file | awk -F "\t" '{OFS="\t";for(i=1;i<=NF;i++){print i,$i}}'
+head -n1 $file | awk -F ${delimiter} '{OFS="\t";for(i=1;i<=NF;i++){print i,$i}}'
